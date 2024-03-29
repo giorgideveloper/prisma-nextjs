59deb95577a0b655018443b3b9d80c2684b9027f
giorgideveloper <gorgiladze888@gmail.com>
2024-03-29 16:08:44 +0400

    "Added Carusel component to Home page"
    This commit message describes the addition of a new component, Carusel, to the Home page. It is concise and accurately reflects the code diff without making any assumptions about the reasons for the change or its implications.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Card from './components/card.component';
+import Carusel from './components/carusel.component';
 
 export default async function Home() {
 	return (
 		<>
 			<div className='container mx-auto'>
+				<Carusel />
 				<div className='grid  lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 justify-center  mt-10 gap-4'>
 					<Card />
 				</div>
